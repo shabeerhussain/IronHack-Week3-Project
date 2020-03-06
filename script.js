5f954f411d69d64220c8c6fca89b30413ff0c1bc
Shabeer Hussain <shabeerhussain@Shabeers-MacBook-Pro-2.local>
2020-03-06 10:45:42 +0100

    Basic Quiz updated

--- script.js
+++ script.js
@@ -19,17 +19,37 @@ let allQuestions = [
     options: ['Madrid', 'Rome', 'Paris'],
     answer: 'Rome',
   },
+  {
+    question: 'What is the capital of the Australia?',
+    options: ['Canberra', 'Sidney', 'Melbourne'],
+    answer: 'Canberra',
+  },
+  {
+    question: 'What is the capital of the Canada?',
+    options: ['Quebec', 'Toronto', 'Ottawa'],
+    answer: 'Ottawa',
+  },
+  {
+    question: 'How many capital cities does South Africa have?',
+    options: ['One', 'Two', 'Three'],
+    answer: 'Three',
+  },
+  {
+    question: 'What is the capital India?',
+    options: ['Mumbai', 'New Delhi', 'Delhi'],
+    answer: 'New Delhi',
+  }
 ];
-
+console.log(allQuestions.length)
 let $q  = document.querySelector('#question'); //initiate variable with current question
 let $b1 = document.querySelector('#option1'); //initiate variable with current first option
 let $b2 = document.querySelector('#option2'); //initiate variable with current second option
 let $b3 = document.querySelector('#option3'); //initiate variable with current third option
 let $result = document.querySelector('#results') //initiate variable with current score
-
 let $optionButtons = document.querySelectorAll('.options'); //initiate variable to store the options
 let questCount = 0;
 let score = 0;
+
 
 function renderQuestions() { // function runs  
   if(questCount<allQuestions.length){ //if the counter is less than the questions in the array
@@ -48,16 +68,16 @@ document.querySelector('#next').addEventListener('click', () => { // listen for 
     $result.innerHTML = yourScore // display result 
     alert (`Your score is ${score}`)
     console.log(score)
+    console.log(allQuestions.length)
   }
-
 });
+
 for (let button of $optionButtons) { // looping through optionsButtons
   button.addEventListener('click', function() { //checking for click then running function
     let ans = allQuestions[questCount - 1].answer; // initiating ans variable to store current answer of the loop 
     if (button.innerHTML === ans) { //if the text buttons matches the variable ans run function
       score++; //increment score
       console.log(score);
-      
     }
   });
 }
